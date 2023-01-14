3b6da1f6ced0f2cd13d38929f68d466d55c63140
Jeison <jeisonhreyes00@gmail.com>
2023-01-13 22:06:50 -0500

    Caracteristicas del FrontEnd

---
+++ frontEnd/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PaymentForms from './PaymentForms.js';
+
+function App() {
+  return (
+      <div className="layout">
+          <PaymentForms />
+      </div>
+  );
+}
+
+export default App;
